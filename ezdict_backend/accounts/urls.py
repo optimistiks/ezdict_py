9ff858f9baa10a802ad51523f6e045b5a19d21f1
optimistiks <optimistiks@gmail.com>
2013-11-12 23:59:33 +0400

    added user login view

--- ezdict_backend/accounts/urls.py
+++ ezdict_backend/accounts/urls.py
@@ -3,13 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from accounts import views
 
 urlpatterns = format_suffix_patterns(patterns('accounts.views',
-    url(r'^users/$',
-        views.UserList.as_view(),
-        name='myuser-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$',
-        views.UserDetail.as_view(),
-        name='myuser-detail'),
-    url(r'^users/(?P<user_pk>\d+)/tickets/$',
-        views.TicketsByUserList.as_view(),
-        name='myuser-tickets'),
+    url(r'^users/$', views.UserList.as_view(), name='myuser-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='myuser-detail'),
+    url(r'^users/(?P<user_pk>\d+)/tickets/$', views.TicketsByUserList.as_view(), name='myuser-tickets'),
+    url(r'^users/login/$', views.UserLogin.as_view()),
 ))
--- ezdict_backend/accounts/views.py
+++ ezdict_backend/accounts/views.py
@@ -3,6 +3,31 @@ from tickets.models import Ticket, TicketCollection
 from accounts.serializers import UserSerializer
 from tickets.serializers import TicketCollectionSerializer
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import ParseError, AuthenticationFailed
+from django.contrib import auth
+
+
+class UserLogin(APIView):
+
+    def post(self, request):
+        username = request.DATA.get('username')
+        password = request.DATA.get('password')
+
+        if username is None or password is None:
+            raise ParseError('Either username or password is not provided')
+        else:
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    auth.login(request, user)
+                    serializer = UserSerializer(user, context={'request': request})
+                    return Response(serializer.data)
+                else:
+                    raise AuthenticationFailed('User is not active')
+            else:
+                raise AuthenticationFailed('Incorrect login or password')
 
 
 class UserList(generics.ListAPIView):
