e0913d846afe1f91f8522760e5c494f87a3b7428
optimistiks <optimistiks@gmail.com>
2013-11-12 23:08:30 +0400

    add ticket tests

--- ezdict_backend/tickets/tests.py
+++ ezdict_backend/tickets/tests.py
@@ -1,16 +1,30 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from accounts.models import MyUser
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TicketTests(APITestCase):
+    def setUp(self):
+        self.user = MyUser.objects.create_user(
+            nickname='testusr', email='testusr@gmail.com', password='qwerty')
+        self.client.login(username=self.user.email, password='qwerty')
+
+    def test_create_ticket(self):
+        url = reverse('ticket-list')
+        data = {'word': 'test ticket', 'tr': 'test tiket', 'text': 'тестовый тикет'}
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['id'], 1)
+        self.assertEqual(response.data['user'], 'http://testserver/users/1/')
+        self.assertEqual(response.data['word'], 'test ticket')
+        self.assertEqual(response.data['tr'], 'test tiket')
+        self.assertIn('text', response.data)
+        self.assertEqual(response.data['liked'], 0)
+        self.assertIn('created', response.data)
+        self.assertIn('updated', response.data)
+
+    def tearDown(self):
+        self.client.logout()
