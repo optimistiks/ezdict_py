eef4121ff7ca96baad7101e7b33751588a7efdb4
optimistiks <optimistiks@gmail.com>
2013-11-19 00:29:48 +0400

    Изменен шаблон url для тикетов ezdict

--- ezdict_backend/ez_tickets/urls.py
+++ ezdict_backend/ez_tickets/urls.py
@@ -3,10 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from ez_tickets import views
 
 urlpatterns = format_suffix_patterns(patterns('tickets.views',
-    url(r'^ezDict_tickets/$',
+    url(r'^ez_tickets/dict/$',
         views.EzDictTicket.as_view(),
-        name='ezDict-ticket'),
-    url(r'^ezTranslate_tickets/$',
+        name='ez_ticket-dict'),
+    url(r'^ez_tickets/translate/$',
         views.EzTranslateTicket.as_view(),
-        name='ezTranslate-ticket'),
+        name='ez_ticket-translate'),
 ))
