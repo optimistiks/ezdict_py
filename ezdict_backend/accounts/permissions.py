d1dade9d91f13d788db676b608e8c1c9d1a2eb7b
optimistiks <optimistiks@gmail.com>
2013-11-20 00:22:55 +0400

    Исправлен баг, что удалить пользователя мог не только админ

--- ezdict_backend/accounts/permissions.py
+++ ezdict_backend/accounts/permissions.py
@@ -16,4 +16,7 @@ class DelByAdminUpdByOwnerOrAdminOtherMethodsForAuthenticated(permissions.BasePe
         if request.method in permissions.SAFE_METHODS:
             return request.user.is_authenticated()
 
-        return request.user.is_staff or obj == request.user
+        if request.method == 'DELETE':
+            return request.user.is_staff
+        else:
+            return request.user.is_staff or obj == request.user
